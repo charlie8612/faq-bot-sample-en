dd2088f80c868d73b23744fd879c07dc3db5273c
charlie8612 <charlie88162@gmail.com>
2018-04-23 11:08:17 +0800

    ABC

--- index.js
+++ index.js
@@ -4,32 +4,60 @@
 ** Import Packages
 */
 const server = require("express")();
+const line = require("@line/bot-sdk");
+const dialogflow = require("apiai-promisified");
+/*--*/
 const bot_express = require("bot-express");
 
+//environment & client config
+const line_config = {
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数
+    channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数Channel Secret
+};
+const bot = new line.Client(line_config);
+
 /*
-** Middleware Configuration
+** web server
 */
-server.listen(process.env.PORT || 5000, () => {
-    console.log("server is running...");
+var server_port = server.listen(process.env.PORT || 3000, function(){
+    var port = server_port.address().port;
+    console.log("App now running on port", port);
 });
 
-/*
-** Mount bot-express
-*/
-server.use("/webhook", bot_express({
-    language: "en",
-    nlu: {
-        options: {
-            client_access_token: process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN,
-            language: "en"
-        }
-    },
-    line_channel_secret: process.env.LINE_CHANNEL_SECRET,
-    line_access_token: process.env.LINE_ACCESS_TOKEN,
-    default_skill: process.env.DEFAULT_SKILL,
-    google_project_id: process.env.GOOGLE_PROJECT_ID,
-    google_api_key: process.env.GOOGLE_API_KEY,
-    auto_translation: process.env.AUTO_TRANSLATION
-}));
+// Dialogflow create
+const nlu = new dialogflow(process.env.DIALOGFLOW_CLIENT_ACCESS_TOKEN, {language: "en"});
+/* web port*/
+server.post("/webhook", line.middleware(line_config), (req, res, next) => {
+    //line server default setting
+    res.sendStatus(200);
+    // array stores promises for all event processing.
+    let events_processed = [];
 
-module.exports = server;
+    if(event.type == 'message' && event.message.type == "text"){
+        events_processed.push(
+            nlu.textRequest(event.message.text, {sessionId: event.source.userId}).then((response) => {
+                if (response.result && response.result.action == "handle-delivery-order"){
+                    let message;
+                    if (response.result.parameters.menu && response.result.parameters.menu != ""){
+                        message = {
+                            type: "text",
+                            text: `毎度！${response.result.parameters.menu}ね。どちらにお届けしましょ？`
+                        }
+                    } else {
+                        message = {
+                            type: "text",
+                            text: `毎度！ご注文は？`
+                        }
+                    }
+                    return bot.replyMessage(event.replyToken, message);
+                }
+            })
+        )
+    }
+    // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+    Promise.all(events_processed).then(
+        (response) => {
+            console.log(`${response.length} event(s) processed.`);
+        }
+    );
+})
